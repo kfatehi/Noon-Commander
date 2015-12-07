8c1999d43f503b56761c3df415bf7e99cc59a4c4
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-12-07 02:36:27 -0800

    init

--- Noon Commander/ViewController.m
+++ Noon Commander/ViewController.m
@@ -24,4 +24,57 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)turnOnLed:(id)sender {
+    [self sendRequest:self];
+}
+
+- (void)sendRequest:(id)sender
+{
+    /* Configure session, choose between:
+     * defaultSessionConfiguration
+     * ephemeralSessionConfiguration
+     * backgroundSessionConfigurationWithIdentifier:
+     And set session-wide properties, such as: HTTPAdditionalHeaders,
+     HTTPCookieAcceptPolicy, requestCachePolicy or timeoutIntervalForRequest.
+     */
+    NSURLSessionConfiguration* sessionConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    /* Create session, and optionally set a NSURLSessionDelegate. */
+    NSURLSession* session = [NSURLSession sessionWithConfiguration:sessionConfig delegate:nil delegateQueue:nil];
+    
+    /* Create the Request:
+     My API (POST http://pi:4000/activate)
+     */
+    
+    NSURL* URL = [NSURL URLWithString:@"http://pi:4000/activate"];
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:URL];
+    request.HTTPMethod = @"POST";
+    
+    // Headers
+    
+    [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    // JSON Body
+    
+    NSDictionary* bodyObject = @{
+                                 @"pin": @"0000"
+                                 };
+    request.HTTPBody = [NSJSONSerialization dataWithJSONObject:bodyObject options:kNilOptions error:NULL];
+    
+    /* Start a new Task */
+    NSURLSessionDataTask* task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error == nil) {
+            // Success
+            NSLog(@"URL Session Task Succeeded: HTTP %ld", ((NSHTTPURLResponse*)response).statusCode);
+        }
+        else {
+            // Failure
+            NSLog(@"URL Session Task Failed: %@", [error localizedDescription]);
+        }
+    }];
+    [task resume];
+}
+
+
+
 @end
